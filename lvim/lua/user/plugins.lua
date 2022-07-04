9e50fc88cdb46288382735101e2211e499177bb5
ArthurJordao <arthurbernardijordao@gmail.com>
2022-07-04 16:28:15 -0300

    Separate lua config into modules

---
+++ lvim/lua/user/plugins.lua
@@ -0,0 +1,14 @@
+lvim.plugins = {
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
+  { "direnv/direnv.vim" },
+  { "sainnhe/gruvbox-material" },
+  { "easymotion/vim-easymotion" },
+  { "mg979/vim-visual-multi" },
+  { "wakatime/vim-wakatime" },
+  { "mattn/emmet-vim" },
+  { "p00f/nvim-ts-rainbow" }
+}
+
---
+++ lvim/lua/user/treesitter.lua
@@ -0,0 +1,25 @@
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "clojure",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "haskell",
+  "elm",
+  "tsx",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+}
+
+lvim.builtin.treesitter.rainbow = {
+  enable = true,
+  extended_mode = true,
+  max_file_lines = nil,
+}
+
+lvim.builtin.treesitter.highlight.enabled = true
