c258e49163ef5ab479cf17663e6dd8b9d4dfda43
ArthurJordao <arthurbernardijordao@gmail.com>
2022-07-06 22:56:22 -0300

    Back to single file config on lvim

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,4 +1,68 @@
-require "user.bindings"
-require "user.treesitter"
-require "user.options"
-require "user.plugins"
+--Bindings
+lvim.leader = "space"
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
+lvim.keys.normal_mode["<leader>r"] = ":NvimTreeRefresh<cr>"
+
+lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Trouble",
+  r = { "<cmd>Trouble lsp_references<cr>", "References" },
+  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
+}
+
+--Plugins
+lvim.plugins = {
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
+  { "direnv/direnv.vim" },
+  { "sainnhe/gruvbox-material" },
+  { "easymotion/vim-easymotion" },
+  { "mg979/vim-visual-multi" },
+  { "wakatime/vim-wakatime" },
+  { "mattn/emmet-vim" },
+  { "p00f/nvim-ts-rainbow" }
+}
+
+--Preferences
+lvim.log.level = "warn"
+lvim.format_on_save = false
+lvim.colorscheme = "gruvbox-material"
+
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.notify.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "right"
+
+--Treesitter
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "clojure",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "haskell",
+  "elm",
+  "tsx",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+}
+
+lvim.builtin.treesitter.rainbow = {
+  enable = true,
+  extended_mode = true,
+  max_file_lines = nil,
+}
+
+lvim.builtin.treesitter.highlight.enabled = true
