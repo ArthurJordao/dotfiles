533a6a5bd194b386f2938739c4e27541d3442819
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2023-04-15 19:43:37 -0300

    Add openingithub to neovim

---
+++ astrovim-config/plugins/openingh.lua
@@ -0,0 +1,7 @@
+return {
+  "almo7aya/openingh.nvim",
+  keys = {
+    { "<leader>go", ":OpenInGHFile <CR>", desc = "Open in github" },
+    { "<leader>go", ":OpenInGHFile <CR>", desc = "Open in github", mode = "v" },
+  }
+}
