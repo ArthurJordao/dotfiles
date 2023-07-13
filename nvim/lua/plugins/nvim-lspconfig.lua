ef9e75845a7bd43324f337c51589090b6c9e2976
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2023-07-13 16:04:56 -0300

    There was already a map for it

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,16 +1,5 @@
 return {
   "neovim/nvim-lspconfig",
-  init = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    keys[#keys + 1] = {
-      "<leader>cs",
-      function()
-        require("telescope.builtin").lsp_document_symbols()
-      end,
-      desc = "Open document symbols",
-      mode = "n",
-    }
-  end,
   opts = {
     autoformat = false,
     servers = {
