72e911eb62a3010f4d4d9e0731188fc4bcae7e27
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2024-01-22 18:23:18 -0300

    Config tailwind on heex files

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -5,6 +5,10 @@ return {
       hls = {
         mason = false,
       },
+     emmet_language_server = {
+        filetypes = {"css", "eruby", "html", "heex", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue"}
+
+      }
     },
   },
 }
