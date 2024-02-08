396dd57d8d45b9d67c1a12957f4c428a4871f5d0
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2024-02-08 10:35:17 -0300

    Add support for unison lang
    This install unison, and also set it up on neovim

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,14 +1,63 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
+
     servers = {
       hls = {
         mason = false,
       },
-     emmet_language_server = {
-        filetypes = {"css", "eruby", "html", "heex", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue"}
+      emmet_language_server = {
+        filetypes = {
+          "css",
+          "eruby",
+          "html",
+          "heex",
+          "javascript",
+          "javascriptreact",
+          "less",
+          "sass",
+          "scss",
+          "svelte",
+          "pug",
+          "typescriptreact",
+          "vue",
+        },
+      },
+      unison = {
+        mason = false,
+        setup = {
+          on_attach = function(client, bufnr)
+            -- Always show the signcolumn, otherwise it would shift the text each time
+            -- diagnostics appear/become resolved
+            vim.o.signcolumn = "yes"
 
-      }
+            -- Update the cursor hover location every 1/4 of a second
+            vim.o.updatetime = 250
+
+            -- Disable appending of the error text at the offending line
+            vim.diagnostic.config({ virtual_text = false })
+
+            -- Enable a floating window containing the error text when hovering over an error
+            vim.api.nvim_create_autocmd("CursorHold", {
+              buffer = bufnr,
+              callback = function()
+                local opts = {
+                  focusable = false,
+                  close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+                  border = "rounded",
+                  source = "always",
+                  prefix = " ",
+                  scope = "cursor",
+                }
+                vim.diagnostic.open_float(nil, opts)
+              end,
+            })
+
+            -- This setting is to display hover information about the symbol under the cursor
+            vim.keymap.set("n", "K", vim.lsp.buf.hover)
+          end,
+        },
+      },
     },
   },
 }
