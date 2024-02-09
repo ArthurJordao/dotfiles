93b5e1ce681ad657db5c15f1e4fa1f6f85cee155
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2024-02-09 10:50:33 -0300

    Use fish shell

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+set -x GPG_TTY $(tty)
+set -x GOPATH "$HOME/go"
+set -x GOROOT "$(/opt/homebrew/bin/brew --prefix golang)/libexec"
+set -x EDITOR nvim
+fish_add_path "/opt/homebrew/bin"
+fish_add_path "$HOME/.local/bin"
+fish_add_path "$HOME/.ghcup/bin"
+fish_add_path "$HOME/.cache/rebar3/bin"
+fish_add_path "$GOPATH/bin"
+fish_add_path "$GOROOT/bin"
+fish_add_path "$HOME/.config/emacs/bin"
+fish_add_path "$HOME/.cargo/bin:$PATH"
+direnv hook fish | source
+fish_config theme choose "Rosé Pine Dawn"
