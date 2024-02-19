218597e7c9fcd6e5508e8082c38faea5c399c963
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2024-02-19 15:24:36 -0300

    Use catpuccin on bat and fzf

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,7 @@ set -x GPG_TTY $(tty)
 set -x GOPATH "$HOME/go"
 set -x GOROOT "$(/opt/homebrew/bin/brew --prefix golang)/libexec"
 set -x EDITOR nvim
+set -x BAT_THEME "Catppuccin-latte"
 fish_add_path "/opt/homebrew/bin"
 fish_add_path "$HOME/.local/bin"
 fish_add_path "$HOME/.ghcup/bin"
@@ -13,3 +14,7 @@ fish_add_path "$HOME/.cargo/bin:$PATH"
 direnv hook fish | source
 fish_config theme choose "Catppuccin Latte"
 fzf_configure_bindings --processes=\cb --directory=\cf --git_log=\ct --git_status=\cs
+set -Ux FZF_DEFAULT_OPTS "\
+--color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39 \
+--color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
+--color=marker:#dc8a78,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39"
