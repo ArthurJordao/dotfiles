bb62bdb1c4744aec446850f6015999c6d240f365
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2024-11-19 18:01:57 -0300

    Add sdkman and zellij

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -1,25 +1,21 @@
 return {
   {
-    "tamton-aquib/duck.nvim",
-    keys = {
-      {
-        "<leader>dd",
-        function()
-          require("duck").hatch('🐤')
-        end,
-        desc = "Duck hatch",
-        mode = "n",
-        silent = true,
-      },
-      {
-        "<leader>dk",
-        function()
-          require("duck").cook()
-        end,
-        desc = "Duck cook",
-        mode = "n",
-        silent = true,
-      },
+    "echasnovski/mini.pairs",
+    event = "VeryLazy",
+    opts = {
+      modes = { insert = true, command = true, terminal = false },
+      -- skip autopair when next character is one of these
+      skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+      -- skip autopair when the cursor is inside these treesitter nodes
+      skip_ts = { "string" },
+      -- skip autopair when next character is closing pair
+      -- and there are more closing pairs than opening pairs
+      skip_unbalanced = false,
+      -- better deal with markdown code blocks
+      markdown = true,
     },
+    config = function(_, opts)
+      LazyVim.mini.pairs(opts)
+    end,
   },
 }
