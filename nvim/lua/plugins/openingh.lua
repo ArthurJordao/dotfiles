18d206046d32e5aeb3736a80a3e858cafc24b35a
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2024-11-22 13:38:23 -0300

    Remove unnecessary plugin

--- nvim/lua/plugins/openingh.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "almo7aya/openingh.nvim",
-  keys = {
-    { "<leader>gO", ":OpenInGHRepo <CR>", desc = "Open repo in github", silent = true },
-    { "<leader>go", ":OpenInGHFile <CR>", desc = "Open file in github", silent = true },
-    { "<leader>go", ":OpenInGHFile <CR>", desc = "Open file in github", mode = "v", silent = true },
-  },
-}
