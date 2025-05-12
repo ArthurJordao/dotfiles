39cd83c610a6e9764e436320a357732ce2bd22a5
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2025-05-12 11:18:17 -0300

    Lazyvim is updated pinning mason version, so I don't need this hack anymoe

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -29,16 +29,4 @@ require("lazy").setup({
       },
     },
   },
-  { "mason-org/mason.nvim", version = "^1.0.0" },
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
-  -- more plugins loaded here ...
-  { -- LSP Configuration & Plugins
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      -- Automatically install LSPs to stdpath for neovim
-      "mason-org/mason.nvim",
-      "mason-org/mason-lspconfig.nvim",
-      ...,
-    },
-  },
 })
