17d34f23e56cafa0c5e75469fc6f88113ef0a442
Arthur Bernardi Jordão <arthurbernardijordao@gmail.com>
2025-05-14 19:07:07 -0300

    Add some keybindings to zk nvim

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -25,7 +25,11 @@ return {
     },
   },
   {
-    "zk-org/zk-nvim",
+    "JulianNymark/zk-nvim",
+    branch = "add-some-guards",
+    dependencies = {
+      "folke/which-key.nvim",
+    },
     config = function()
       require("zk").setup({
         picker = "fzf_lua",
@@ -34,6 +38,50 @@ return {
           config = {
             cmd = { "zk", "lsp" },
             name = "zk",
+            on_attach = function()
+              local cmd = require("zk.commands")
+
+              local wk = require("which-key")
+              wk.add({
+                mode = { "n", "v" },
+                { "<leader>z", group = "Zk" },
+              })
+
+              --visual mode
+              vim.keymap.set(
+                "n",
+                "<leader>zn",
+                ":ZkNew {title = vim.fn.input('Title: '), dir = vim.fn.input('Dir: ')}<cr>",
+                { desc = "New note" }
+              )
+              vim.keymap.set("n", "<leader>zt", ":ZkTags<cr>", { desc = "All tags" })
+              vim.keymap.set("n", "<leader>zf", ":ZkNotes {excludeHrefs = {'journal'}}<cr>", { desc = "List notes" })
+              vim.keymap.set("n", "<leader>zo", ":ZkLinks<cr>", { desc = "List links" })
+              vim.keymap.set("n", "<leader>zs", ":ZkBuffers<cr>", { desc = "List buffers" })
+              vim.keymap.set("n", "<leader>zi", ":ZkBacklinks<cr>", { desc = "List backlinks" })
+
+              -- visual mode
+              vim.keymap.set("v", "<leader>zm", ":ZkMatch<cr>", { desc = "Match selection content" })
+              vim.keymap.set(
+                "v",
+                "<leader>zn",
+                ":ZkNewFromTitleSelection {dir = vim.fn.input('Dir: ')}<cr>",
+                { desc = "New note from title" }
+              )
+              vim.keymap.set("v", "<leader>zl", ":ZkInsertLinkAtSelection<cr>", { desc = "Insert link at selection" })
+              vim.keymap.set(
+                "v",
+                "ze",
+                ":ZkNewFromContentSelection {title = vim.fn.input('Title: '), dir = vim.fn.input('Dir: ')}<cr>",
+                { desc = "New note from content" }
+              )
+
+              -- insert mode
+              vim.keymap.set("i", "<c-r>", function()
+                vim.cmd("norm! i") -- otherwise link gets inserted after cursor
+                cmd.get("ZkInsertLink")()
+              end)
+            end,
             -- on_attach = ...
             -- etc, see `:h vim.lsp.start_client()`
             auto_attach = {
